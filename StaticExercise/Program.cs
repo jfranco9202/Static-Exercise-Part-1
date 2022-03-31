459d06990003d2440bb6d0a73ab5cd8ea431c7f8
jfranco9202 <jeremy.franco41@yahoo.com>
2022-03-31 08:31:06 -0500

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var celsius = TempConverter.FahrenheitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"The result");
+            Console.WriteLine($"celcius {celsius}");
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+
+
+
+        }
+    }
+}
